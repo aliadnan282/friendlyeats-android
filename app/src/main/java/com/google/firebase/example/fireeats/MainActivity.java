528dfa0385d93226799991672689468ee83c10e2
Adnan Ali <adnan.ali@logicon.com.pk>
2019-06-01 12:56:21 +0500

    read write successfully added

--- app/src/main/java/com/google/firebase/example/fireeats/MainActivity.java
+++ app/src/main/java/com/google/firebase/example/fireeats/MainActivity.java
@@ -1,30 +1,30 @@
 /**
  * Copyright 2017 Google Inc. All Rights Reserved.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- package com.google.firebase.example.fireeats;
+package com.google.firebase.example.fireeats;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
-import android.util.JsonReader;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -34,6 +34,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.example.fireeats.adapter.RestaurantAdapter;
 import com.google.firebase.example.fireeats.model.Plan;
@@ -41,12 +44,17 @@ import com.google.firebase.example.fireeats.model.PlanModel;
 import com.google.firebase.example.fireeats.util.JsonManager;
 import com.google.firebase.example.fireeats.viewmodel.MainActivityViewModel;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -75,7 +83,8 @@ public class MainActivity extends AppCompatActivity implements
     RecyclerView mRestaurantsRecycler;
 
     @BindView(R.id.view_empty)
-    ViewGroup mEmptyView;;
+    ViewGroup mEmptyView;
+    ;
 
     private FirebaseFirestore mFirestore;
     private Query mQuery;
@@ -84,6 +93,9 @@ public class MainActivity extends AppCompatActivity implements
     private RestaurantAdapter mAdapter;
 
     private MainActivityViewModel mViewModel;
+
+    PlanModel planModel=new PlanModel();
+    List<Plan> plan=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -110,6 +122,7 @@ public class MainActivity extends AppCompatActivity implements
         mFirestore = FirebaseFirestore.getInstance();
         FirebaseFirestore.setLoggingEnabled(true);
     }
+
     private void initRecyclerView() {
         if (mQuery == null) {
             Log.w(TAG, "No query, not initializing RecyclerView");
@@ -170,9 +183,10 @@ public class MainActivity extends AppCompatActivity implements
 
     private void onAddItemsClicked() {
         CollectionReference workout = mFirestore.collection("workout");
-        PlanModel planModel= JsonManager.getInstance(this).getPlanExercise();
+        PlanModel planModel = JsonManager.getInstance(this).getPlanExercise();
         for (Plan plan : planModel.getPlan()) {
             workout.add(plan);
+            //workout.document("plan" + (plan.getPlanId())).set(plan);
         }
     }
 
@@ -202,8 +216,33 @@ public class MainActivity extends AppCompatActivity implements
                 onAddItemsClicked();
                 break;
             case R.id.menu_sign_out:
-                AuthUI.getInstance().signOut(this);
-                startSignIn();
+               /* AuthUI.getInstance().signOut(this);
+                startSignIn();*/
+               /* DocumentReference docRef =  mFirestore.collection("workout").document("plan1");
+                docRef.get()
+                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>(){
+
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                Plan taskList= documentSnapshot.toObject(Plan.class);
+                                Log.d(TAG, taskList.getPlanId() + " => " +taskList.getDay().get(0).getDayId());
+                            }
+                        });*/
+
+
+                mFirestore.collection("workout")
+                        .get()
+                        .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                            @Override
+                            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                 List<DocumentSnapshot> taskList= queryDocumentSnapshots.getDocuments();
+                                for (DocumentSnapshot documentSnapshot:        taskList) {
+                                    plan.add(documentSnapshot.toObject(Plan.class));
+                                }
+                                planModel.setPlan(plan);
+                                Log.d(TAG, "onSuccess: "+planModel.getPlan().size());
+                            }
+                        });
                 break;
         }
         return super.onOptionsItemSelected(item);
